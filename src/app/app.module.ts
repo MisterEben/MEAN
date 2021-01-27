350070f0dee5e063789bb8bc8271d46b0d0e05c7
misterEben <lanybenblogs45@gmail.com>
2021-01-27 10:35:53 +0000

    angular-src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardsComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
